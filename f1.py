eec05b08faabd12601b1e717aaf2fd08888a113e
lakshita <lakshita8899@gmail.com>
2025-04-04 23:03:46 +0530

    added fl.py

---
+++ f1.py
@@ -0,0 +1,15 @@
+C:\Users\DELL\Documents\mini\New folder\new git repo\new>git checkout -b ML/laks
+fatal: cannot lock ref 'refs/heads/ML/laks': 'refs/heads/ML' exists; cannot create 'refs/heads/ML/laks'
+
+C:\Users\DELL\Documents\mini\New folder\new git repo\new>git checkout -b DL/laks
+Switched to a new branch 'DL/laks'
+
+C:\Users\DELL\Documents\mini\New folder\new git repo\new>git branch
+* DL/laks
+  ML
+  main
+
+C:\Users\DELL\Documents\mini\New folder\new git repo\new>git pull origin main
+From https://github.com/Lakshita8899/new
+ * branch            main       -> FETCH_HEAD
+Already up to date.
